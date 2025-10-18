666c46c49821f0483b951392ff0714603e814925
Alex <alexjf2912@gmail.com>
2025-10-18 18:01:27 +0200

    feature(optimize_entity): optimized table in ddbb
    Optimized the table in ddbb by adding indexes related to the query.

--- src/main/java/com/example/appprueba/adapters/out/jpa/entity/PriceEntity.java
+++ src/main/java/com/example/appprueba/adapters/out/jpa/entity/PriceEntity.java
@@ -1,15 +1,14 @@
 package com.example.appprueba.adapters.out.jpa.entity;
 
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import jakarta.persistence.Table;
-import jakarta.persistence.GenerationType;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
+import jakarta.persistence.Id;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -32,14 +31,29 @@ import java.time.LocalDateTime;
 @Builder
 public class PriceEntity {
 
-    private Long brandId;
-    private LocalDateTime startDate;
-    private LocalDateTime endDate;
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "price_list")
     private Long priceList;
+
+    @Column(name = "brand_id", nullable = false)
+    private Long brandId;
+
+    @Column(name = "start_date", nullable = false)
+    private LocalDateTime startDate;
+
+    @Column(name = "end_date", nullable = false)
+    private LocalDateTime endDate;
+
+    @Column(name = "product_id", nullable = false)
     private Long productId;
+
+    @Column(name = "priority", nullable = false)
     private Integer priority;
+
+    @Column(name = "price", nullable = false, precision = 10, scale = 2)
     private BigDecimal price;
+
+    @Column(name = "curr", nullable = false)
     private String curr;
+
 }
